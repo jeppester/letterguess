667eaa31239f7c763b1c9cfe61514a06178f3e94
Jesper Sørensen <jeppester1@gmail.com>
2023-06-15 21:58:32 +0200

    Setup deployment through git hook

---
+++ public/deployment-hook.php
@@ -0,0 +1,10 @@
+<?php
+
+$secret = file_get_contents(dirname(__FILE__) . '/../deployment-secret');
+$request_body = file_get_contents('php://input');
+$computed_hmac = hash_hmac('sha1', $request_body, $secret);
+$send_hmac = substr($_SERVER['HTTP_X_HUB_SIGNATURE'], 5);
+
+if ($send_hmac !== $computed_hmac) exit;
+
+shell_exec(dirname(__FILE__) . '/../deployment-hook.sh');
