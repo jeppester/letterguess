83aaf73a1378b16cfa24015b991249b801b6ac4f
Jesper Sørensen <jeppester1@gmail.com>
2023-06-15 22:17:45 +0200

    Make deployment hook trim secret

--- public/deployment-hook.php
+++ public/deployment-hook.php
@@ -1,6 +1,6 @@
 <?php
 
-$secret = file_get_contents(dirname(__FILE__) . '/../deployment-secret');
+$secret = trim(file_get_contents(dirname(__FILE__) . '/../deployment-secret'));
 $request_body = file_get_contents('php://input');
 $computed_hmac = hash_hmac('sha1', $request_body, $secret);
 $send_hmac = substr($_SERVER['HTTP_X_HUB_SIGNATURE'], 5);
